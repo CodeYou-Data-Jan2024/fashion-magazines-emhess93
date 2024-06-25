0589d04e9bcf8e813b9aa3dd4432cdc453035cbd
Emily Hess <emilynhess93@gmail.com>
2024-06-24 23:03:04 -0400

    partial

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,13 @@
--- Add your sql here
+--join customers table to orders to get customer name
+SELECT orders.*, customers.customer_name, subscriptions.*
+FROM orders
+Left JOIN customers
+LEFT JOIN subscriptions
+WHERE order_status is 'unpaid' AND description is 'Fashion Magazine';
+
+
+
+--multiply subs price with sub length to get total due
+--format the total amount as currency with PRINTF()
+*/
+--Group by Customer and sum the amount due to account for cust with >1 unpaid fashion mag
